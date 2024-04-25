1fc557e7f7b29e0a510101d80bbc5fd911df6922
GClabaut <123895070+GClabaut@users.noreply.github.com>
2024-04-25 11:31:39 +0200

    Finished rock-paper-scissor

--- main.js
+++ main.js
@@ -1,6 +1,56 @@
 const startGame = document.querySelector('.input');
-const result = document.querySelector('.result');
 const score = document.querySelector('.score');
+const result = document.querySelector('.result');
+
+let win = 0;
+let lose = 0;
+
+function playRound(getPlayerChoice) {
+    if (getPlayerChoice === 'rock') {
+        if (getComputerChoice() === 'rock') {
+            const resultTxt = 'Draw';
+            result.innerHTML = resultTxt;
+        } else if (getComputerChoice() === 'paper') {
+            const resultTxt = 'You lose';
+            lose ++;
+            result.innerHTML = resultTxt;
+        } else {
+            const resultTxt = 'You win';
+            win ++;
+            result.innerHTML = resultTxt;
+        }
+    } else if (getPlayerChoice === 'paper') {
+        if (getComputerChoice() === 'rock') {
+            const resultTxt = 'You win';
+            win ++;
+            result.innerHTML = resultTxt;
+        } else if (getComputerChoice() === 'paper') {
+            const resultTxt = 'Draw';
+            result.innerHTML = resultTxt;
+        } else {
+            const resultTxt = 'You lose';
+            lose ++;
+            result.innerHTML = resultTxt;
+        }
+    } else if (getPlayerChoice === 'scissor') {
+        if (getComputerChoice() === 'rock') {
+            const resultTxt = 'You lose';
+            lose ++;
+            result.innerHTML = resultTxt;
+        } else if (getComputerChoice() === 'paper') {
+            const resultTxt = 'You win';
+            win ++;
+            result.innerHTML = resultTxt;
+        } else {
+            const resultTxt = 'Draw';
+            result.innerHTML = resultTxt;
+        }
+    } else {
+        result.innerHTML = "Please choose 'rock', 'paper' or 'scissor'";
+    }
+
+    score.innerHTML = `You - ${win} | ${lose} - Computer`
+}
 
 function getComputerChoice() {
     const rng = Math.floor(Math.random() * 3);
@@ -12,9 +62,12 @@ function getComputerChoice() {
         return 'scissor';
     }
 }
-console.log(getComputerChoice())
 
 startGame.addEventListener('submit', (e) => {
     e.preventDefault();
-    const getPlayerChoice = e.target[0].value
+
+    const getPlayerChoice = e.target[0].value.toLowerCase();
+
+    playRound(getPlayerChoice);
+
 })
